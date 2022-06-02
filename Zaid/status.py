f7611f293e6f8f4ac062541870f854958fcf3ba8
Zedkindevil <94899615+Zedkindevil@users.noreply.github.com>
2022-06-02 11:15:37 +0530

    Update status.py

--- Zaid/status.py
+++ Zaid/status.py
@@ -14,5 +14,5 @@ def is_admin(func):
         if is_admin:
             await func(event, _s)
         else:
-            await event.reply("teri bass ki nahi hai admeme ko bol!")
+            await event.reply("teri bass ki nahi hai admeme ko bol! 🤣")
     return a_c
